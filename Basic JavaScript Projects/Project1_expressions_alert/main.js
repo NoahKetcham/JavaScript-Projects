184f684c0ef68f4cb77ef726bc6e9deb36b1d1a7
NoahKetcham <150949337+NoahKetcham@users.noreply.github.com>
2023-12-11 10:42:01 -0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

--- Basic JavaScript Projects/Project1_expressions_alert/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -1,26 +1,29 @@
 
 
-document.write('Hello, World!');
+document.write('Hello, World!'); //prints the text within ()
 
-var a = "this is a string";
+var a = "this is a string"; // sets a variable
 
-document.write(a)
+document.write(a) // prints a set variable
 
 var B = "this is the string's\ assignment \\\\";
 
-window.alert(B);
+window.alert(B);  //creates a window alert when initially loading a page
 
 document.write ("\"Be who you are and say what you feel,"
 + " because those who mind don\'t matter and those who matter don\'t mind.\""
-+ "-Dr. Suess");
++ "-Dr. Suess");  //concatenated strings,  different sets of data added together 
 
-var C = "i like cars" + "I like food";
+var C = "i like cars" + "I like food";  //concatenated variable
 
-document.write(C)
+document.write(C) 
 
 var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
     Daughter = "Penny", Son = "Zorro";
 
 document.write(Dad)
 
-document.write (3+3)
+document.write (3+3) //prints an expression , this will add 3 plus 3
+
+//sdfdfsdf
+
