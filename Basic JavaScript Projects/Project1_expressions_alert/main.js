f91143d9a696c9b811238c0659698abd3577f4f6
NoahKetcham <150949337+NoahKetcham@users.noreply.github.com>
2023-12-11 10:41:45 -0800

    BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project1_expressions_alert/main.js
@@ -0,0 +1,26 @@
+
+
+document.write('Hello, World!');
+
+var a = "this is a string";
+
+document.write(a)
+
+var B = "this is the string's\ assignment \\\\";
+
+window.alert(B);
+
+document.write ("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Suess");
+
+var C = "i like cars" + "I like food";
+
+document.write(C)
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine",
+    Daughter = "Penny", Son = "Zorro";
+
+document.write(Dad)
+
+document.write (3+3)
