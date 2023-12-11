1f51278041e3ee424123321fc6ebb4f051e71acb
NoahKetcham <150949337+NoahKetcham@users.noreply.github.com>
2023-12-11 11:54:20 -0800

    PROJECT2_FUNCTIONS SUBMISSION ASSIGNMENT

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+var A = " Hello"
+var B = " World"
+
+//function My_First_Function() {
+//    var str = "this is my button text!";
+//    document.getElementById("Button_Text").innerHTML = str + A + B;
+//}
+
+function myFunction() {
+    var sentence = " I am learning"; //setting variable and first section of string input
+    sentence += " a lot from this book!"; //Concatenating both strings into the variable
+    document.getElementById("Concatenate").innerHTML = sentence; //Calls to the html doc?
+}
